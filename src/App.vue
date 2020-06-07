861c03d39cb296f0b155204cfb7053cc27ca44fe
mills <milindvishnoi@gmail.com>
2020-06-07 14:11:12 +0530

    Navbar completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <navbar />
+    <hello-world />
+    <div style="height: 10000px;" />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue"
+import HelloWorld from "./components/HelloWorld.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+      HelloWorld,
+      Navbar
+  },
+
+  data: () => ({
+  }),
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <v-card
+    color="grey lighten-4"
+    flat
+    tile
+  >
+    <v-toolbar dense min-height="52px">
+        <v-btn x-large style="margin: 2px;" text color="secondary">
+          Home
+        </v-btn>
+
+        <v-btn x-large style="margin: 2px;"  text color="secondary">
+          About Me
+        </v-btn>
+
+        <v-btn x-large style="margin: 2px;"  text color="secondary">
+          Projects
+        </v-btn>
+      <v-spacer />
+      <v-btn icon color="secondary" style="margin: 2px;" href="https://www.instagram.com/confessions.of.a.coding.freak/">
+        <v-icon x-large>mdi-instagram</v-icon>
+      </v-btn>
+      <v-btn icon color="secondary" style="margin: 2px;" href="https://github.com/milindvishnoi">
+        <v-icon x-large>mdi-github</v-icon>
+      </v-btn>
+      <v-btn icon color="secondary" style="margin: 2px;">
+        <v-icon x-large>mdi-weather-sunny</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+text-color {
+  color: secondary;
+}
+</style>
