de7f989da8feced4462d3cea05d8860f767bc642
mills <milindvishnoi@gmail.com>
2020-06-09 09:44:54 +0530

    Added reactivity to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
     <v-card
+    fixed
     color="grey lighten-4"
     flat
     tile
     >
     <!-- For Mobiles -->
+    <v-toolbar v-if="getMobile" :color="getDarkMode ? 'none' : 'primary'" dense min-height="52px">
+      <v-spacer />
+        <v-btn text x-large color="headings">
+          <v-icon x-large>mdi-home</v-icon>
+        </v-btn>
+      <v-spacer />
+      <!-- <v-btn icon color="headings" style="margin: 2px;" href="https://www.instagram.com/confessions.of.a.coding.freak/">
+        <v-icon x-large>mdi-instagram</v-icon>
+      </v-btn>
+      <v-btn icon color="headings" style="margin: 2px;" href="https://github.com/milindvishnoi">
+        <v-icon x-large>mdi-github</v-icon>
+      </v-btn>
+      <v-btn icon color="headings" style="margin: 2px;" href="https://www.linkedin.com/in/milindvishnoi/">
+        <v-icon x-large>mdi-linkedin</v-icon>
+      </v-btn> -->
+      <!-- Disabled Light Mode -->
+      <!-- <v-btn v-if="getDarkMode" @click="$vuetify.theme.dark = ! $vuetify.theme.dark" icon color="headings" style="margin: 2px;">
+        <v-icon x-large>mdi-weather-sunny</v-icon>
+      </v-btn>
+      <v-btn v-else @click="$vuetify.theme.dark = ! $vuetify.theme.dark" icon color="headings" style="margin: 2px;">
+        <v-icon x-large>mdi-weather-night</v-icon>
+      </v-btn> -->
+    </v-toolbar>
     <!-- For Laptops   -->
-    <v-toolbar :color="getDarkMode ? 'none' : 'primary'" dense min-height="52px">
+    <v-toolbar v-else :color="getDarkMode ? 'none' : 'primary'" dense min-height="52px">
         <v-btn x-large text color="headings">
           Home
         </v-btn>
@@ -47,6 +71,11 @@
 
 <script>
     export default {
+      data() {
+        return {
+          overlay: false
+        }
+      },
       computed: {
         getDarkMode: {
           get: function() {
@@ -55,6 +84,12 @@
           set: function(newValue) {
             this.$vuetify.theme.data = newValue;
           }
+        },
+        getMobile() {
+          if(this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs) {
+            return true;
+          }
+          return false;
         }
       },
     }
